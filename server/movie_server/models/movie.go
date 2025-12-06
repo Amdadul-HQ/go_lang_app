b4ef44b45a3821e8601213777aa52b52c86dd43b
Amdadul <amdadul.digipylot@gmail.com>
2025-12-06 11:55:30 +0600

    Refactor Movie model to include BSON and JSON struct tags for improved serialization

--- server/movie_server/models/movie.go
+++ server/movie_server/models/movie.go
@@ -1,27 +1,26 @@
 package model
 
-import(
+import (
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type Genre struct {
-	GenreID int
-	GenreName string
+	GenreID   int    `bson:"genre_id" json:"genre_id"`
+	GenreName string `bson:"genre_name" json:"genre_name"`
 }
 
 type Ranking struct {
-	RankingValue int
-	RankingName string
-
+	RankingValue int    `bson:"ranking_value" json:"ranking_value"`
+	RankingName  string `bson:"ranking_name" json:"ranking_name"`
 }
 
 type Movie struct {
-	ID bson.ObjectID `bson:_id json:"id"`
-	ImdbID string `bson:"imdb_id" json:"imdb_id"`
-	Title string `bson:"title" json:"title"`
-	PosterPath string `bson:"poster_path" json:"poster_path"`
-	YouTubeID string `bson:"youtube_id" json:"youtube_id"`
-	Genre []Genre `bson:"genre" json:"genre"`
-	AdminReview string `bson:"admin_review" json:"admin_review"`
-	Ranking Ranking `bson:"ranking" json:"ranking"`
+	ID          bson.ObjectID `bson:_id json:"_id"`
+	ImdbID      string        `bson:"imdb_id" json:"imdb_id"`
+	Title       string        `bson:"title" json:"title"`
+	PosterPath  string        `bson:"poster_path" json:"poster_path"`
+	YouTubeID   string        `bson:"youtube_id" json:"youtube_id"`
+	Genre       []Genre       `bson:"genre" json:"genre"`
+	AdminReview string        `bson:"admin_review" json:"admin_review"`
+	Ranking     Ranking       `bson:"ranking" json:"ranking"`
 }
