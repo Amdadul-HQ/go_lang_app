467f9b986d5785383e5a514d7e8caf297f2f09f2
Amdadul <amdadul.digipylot@gmail.com>
2025-12-06 11:01:04 +0600

    server route define

--- server/movie_server/main.go
+++ server/movie_server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	router := gin.Default()
+
+	// API routes
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello Movie Server")
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
 }
