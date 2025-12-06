dd788fe08c7545979353531272456792b9c66b3c
Amdadul <amdadul.digipylot@gmail.com>
2025-12-06 11:53:59 +0600

    type declar in bson and json

---
+++ server/movie_server/models/movie.go
@@ -0,0 +1,27 @@
+package model
+
+import(
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Genre struct {
+	GenreID int
+	GenreName string
+}
+
+type Ranking struct {
+	RankingValue int
+	RankingName string
+
+}
+
+type Movie struct {
+	ID bson.ObjectID `bson:_id json:"id"`
+	ImdbID string `bson:"imdb_id" json:"imdb_id"`
+	Title string `bson:"title" json:"title"`
+	PosterPath string `bson:"poster_path" json:"poster_path"`
+	YouTubeID string `bson:"youtube_id" json:"youtube_id"`
+	Genre []Genre `bson:"genre" json:"genre"`
+	AdminReview string `bson:"admin_review" json:"admin_review"`
+	Ranking Ranking `bson:"ranking" json:"ranking"`
+}
